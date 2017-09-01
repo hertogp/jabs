84fd26dcfa426ba2f18fb06bf9bd01ff979ce7ae
hertogp <git.hertogp@github.com>
2017-09-01 16:21:14 +0200

    first working version, except for iplookup and fwr

--- xcol.py
+++ xcol.py
@@ -53,6 +53,13 @@ def add_cmd(cmd=None, *, name=None):
     return cmd
 
 #-- helpers
+def msg(level, *words, output=sys.stderr):
+    'possibly print stuff'
+    if level > args.v:
+        return
+    print(' '.join(str(x) for x in words), file=output)
+
+
 def normalize(name):
     'normalize a column name to something sane'
     name = name.replace('.', '_')     # no dots
@@ -96,15 +103,8 @@ def load_csv(filename):
 
 def write_csv(df, output=sys.stdout):
     'output df to sys.stdout or a file'
-    df.to_csv(output, mode='w')
+    df.to_csv(output, index=False, mode='w')
     return 0
-
-
-def msg(level, *words):
-    'possibly print stuff'
-    if level > args.v:
-        return
-    print(' '.join(str(x) for x in words), file=sys.stdout)
 
 
 def parsecmd(command):
@@ -169,29 +169,45 @@ def parseargs():
 
     return arg
 
-def show_info():
-    msg(0, '\n' + '-'*30, args.prog, 'invocation')
+def show_info(df=None):
+    msg(0, '\n' + '-'*60, args.prog, 'info\n')
+
+    msg(0, '-'*30, 'flags\n')
     msg(0, 'verbosity level (-v) ', args.v)
     msg(0, 'input file      (-i) ', args.i)
     msg(0, 'output file     (-o) ', args.o)
-    msg(0, '\n' + '-'*30, 'commands')
-    maxl = max(len(c) for c in args.commands)
-    for idx, txt in enumerate(args.commands):
-        cmd, dst, arg = args.cmds[idx]
-        c = CMDS.get(cmd, None)
-        if c:
-            msg(0, 'cmd {:02}'.format(idx),
-                '{:{w}}  => {}({},{}) '.format(txt, c.__name__, dst,
-                                              repr(arg), w=maxl))
-        else:
-            msg(0, 'cmd {:02}'.format(idx),
-                '{:{w}}  => ip lookup: {}({},{})'.format(txt, cmd, dst,
-                                                        repr(arg), w=maxl))
 
-    msg(0, '\n' + '-'*30, 'available cmds')
+    if len(args.commands):
+        msg(0, '\n' + '-'*30, 'cli commands\n')
+        maxl = max(len(c) for c in args.commands)
+        for idx, txt in enumerate(args.commands):
+            cmd, dst, arg = args.cmds[idx]
+            c = CMDS.get(cmd, None)
+            if c:
+                msg(0, 'cmd {:02}'.format(idx),
+                    '{:{w}}  => {}({},{}) '.format(txt, c.__name__, dst,
+                                                  repr(arg), w=maxl))
+            else:
+                msg(0, 'cmd {:02}'.format(idx),
+                    '{:{w}}  => ip lookup: {}({},{})'.format(txt, cmd, dst,
+                                                            repr(arg), w=maxl))
+
+    msg(0, '\n' + '-'*30, 'available cmds\n')
     for k, v in sorted(CMDS.items()):
         msg(0, v.__doc__)
 
+    if df is not None:
+        msg(0, '\n' + '-'*30, 'DataFrame\n')
+        msg(0, '{} rows by {} columns'.format(*df.shape))
+        maxl = max(len(c) for c in df.columns.values)+2  # +2 for !r quotes
+        msg(0, '{:{w}} - {}'.format('Column', 'DataType', w=maxl))
+        for col in df.columns:
+            msg(0, '{!r:{w}}   {}'.format(col, df[col].dtype, w=maxl))
+        msg(0, '{:{w}}   {}'.format('<index>', df.index.dtype, w=maxl))
+        msg(0, '\nFirst 3 rows')
+        msg(0, df.head(3))
+
+    msg(0, '\n' + '-'*60, 'info end', '\n')
 #-- commands
 
 @add_cmd
@@ -280,15 +296,114 @@ def join(df, dst, *fields):
 
 
 @add_cmd(name='sum')
-def o_sumf(df, *fields):
-    'sumf:field - sums a field (int valued) across similar rows'
-    print('sumf', *fields)
+def sumf(df, dst, *fields):
+    'dst=sum:[f1,f2,..] - sumsi/counts across similar column groups or rows'
+    if dst is None:
+        msg(0, cmd_str('sum', dst, fields), '<< failed')
+        msg(0, '- dst field required: in dst=sum:[f1,f2,..]')
+        sys.exit(1)
+
+    if dst not in df.columns:
+        df[dst] = 1
+    elif df[dst].dtype not in ['int64', 'float64']:
+        msg(0, cmd_str('sum', dst, fields), '<< failed')
+        msg(0, 'looks like "{}" does not contain numbers to sum'.format(dst))
+        msg(0, 'Columns types are')
+        for name, typ in df.dtypes.items():
+            msg(0, '-', name, typ)
+        sys.exit(1)
+
+    fields = list(x for x in fields if len(x))
+    if len(fields) < 1:
+        fields = df.columns.values
+
+    try:
+        cols = [c for c in fields if c != dst]
+        df = df.groupby(cols, as_index=False).agg({dst: 'sum'})
+    except ValueError as e:
+        msg(0, cmd_str('sum', dst, fields), '<< failed')
+        msg(0, '-', repr(e))
+    except KeyError as e:
+        fatal_field_error(df, 'sum', dst, fields)
+        sys.exit(1)
     return df
 
 @add_cmd
-def o_rgx(df, *fields):
-    'field1=field2~s/abc/ABC/i or field2~/ABC/ to substitue or filter'
-    print('rgx', *fields)
+def rgx(df, dst, *fields):
+    'dst=src~[s]/abc/ABC/[i] or field~/ABC/ to substitue or filter'
+    msg(0, cmd_str('rgx', dst, fields))
+
+    if len(fields) == 1:
+        print(1, fields)
+        src, dst = dst, None
+        expression = fields[0]
+    elif len(fields) == 2:
+        print(2, fields)
+        src = fields[0]
+        expression = fields[1]
+    else:
+        msg(0, cmd_str('rgx', dst, fields), '<< failed')
+        msg(0, '- hmmm, parsing is a bitch')
+        sys.exit(1)
+
+    parts = re.split('(/)', expression)
+    delim = parts[1::2]
+    terms = list(parts[::2])
+
+    print('src', src)
+    print('dst', dst)
+    print('parts', parts)
+    print('slash', delim)
+    print('terms', terms)
+    flags = 0
+    for f in terms[-1]:
+        f = f.lower()
+        if f == 'i':
+            flags |= re.I
+        elif f == 'a':
+            flags |= re.A
+        elif f == 's':
+            flags |= re.S
+        elif f == 'm':
+            flags |= re.M
+        else:
+            msg(0, cmd_str('rgx', src, fields), '<< failed')
+            msg(0, 'unknown flags in {}'.format(terms[-1]))
+            msg(0, 'known flags are  {}'.format('iasm'))
+            msg(0, '-i ignore case')
+            msg(0, '-a make \w, \W .. perform ascii only matching')
+            msg(0, '-s make . match newline as well (not so useful here)')
+            msg(0, '-m make ^ match beginning of line/string')
+            sys.exit(1)
+
+
+    if len(delim) == 2:
+        if dst is None:
+            msg(1, "- filter by matching '{}' on r\'{}'".format(src, terms[1]))
+            prog = re.compile(terms[1], flags)
+            df = df[df[src].apply(lambda x: bool(prog.search(x)))]
+
+        else:
+            msg(1, '- assign {} to {} when it matches {}'.format(src, dst,
+                                                                terms[1]))
+            msg(0, 'not implemented yet')
+            sys.exit(1)
+
+    elif len(delim) == 3:
+        if dst is None:
+            msg(0, '- substitute in {} by {}'.format(src, terms[1:3]))
+            msg(0, 'not implemented yet')
+            sys.exit(1)
+        else:
+            msg(0, '- substitute in {} by {} and assign to {}'.format(src,
+                                                                    terms[1:3],
+                                                                    dst))
+            msg(0, 'not implemented yet')
+            sys.exit(1)
+    else:
+        msg(0, cmd_str('rgx', dst, fields), '<< failed')
+        msg(0, '- dunno what to do with this')
+
     return df
 
 @add_cmd
@@ -299,7 +414,7 @@ def o_fwr(df, *fields):
 
 @add_cmd
 def nan(df, dst, *fields):
-    '[dst=]nan:s1,s2,.. - replace s<x> with null value for dst or all fields'
+    '[dst=]nan:s1,.. - replace values s<x> with null value in dst/all fields'
     try:
         if dst is None:
             for replacer in fields:
@@ -314,10 +429,30 @@ def nan(df, dst, *fields):
 
     return df
 
+
 @add_cmd
-def o_fillby(df, dst, *fields):
-    'fieldx=fillby:fieldy - fills fieldx NaNs via fieldy->fieldx map'
-    print('fillby, *fields')
+def maps(df, src, *dst):
+    'src=maps:f1,f2,.. - ffil/bfill f-columns using src control column'
+    if src is None or src not in df.columns:
+        msg(0, cmd_str('maps', src, dst), '<< failed')
+        msg(0, '- incorrect source control column {}'.format(src))
+        sys.exit(1)
+
+    unknowns = [c for c in dst if c not in df.columns]
+    if len(unknowns):
+        msg(0, cmd_str('maps', src, dst), '<< failed')
+        msg(0, '- unknown dst columns {}'.format(repr(unknowns)))
+        msg(0, '- known columns are:  {}'.format(df.columns.values))
+        sys.exit(1)
+
+    dst = [c for c in dst if c != src]  # avoid source control column
+    fix = df.set_index(src)             # src value mappings to other columns
+    msg(2, '- control column {}'.format(src))
+    for col in dst:
+        dct = fix[[col]].dropna().to_dict()[col]  # null's should be NaNs!
+        msg(2, '- mapping to', repr(col), 'with', len(dct), 'unique mappings')
+        df[col] = df[src].map(dct)
+
     return df
 
 
@@ -347,7 +482,11 @@ def keep(df, dst, *fields):
         sys.exit(1)
     fields = list(fields)
     try:
+        msg(1, '- before: {} rows x {} columns: {}'.format(*df.shape,
+                                                           df.columns.values))
         df = df[fields]
+        msg(1, '- after : {} rows x {} columns: {}'.format(*df.shape,
+                                                       df.columns.values))
     except (KeyError, ValueError):
         fatal_field_error(df, 'del', None, fields)
         sys.exit(1)
@@ -357,11 +496,11 @@ def keep(df, dst, *fields):
 
 def main():
     msg(0)
-    show_info()
     msg(0)
     df = load_csv(args.i)
+    show_info(df)
     for cmd, dst, arg  in args.cmds:
-        msg(0, 'exec:', cmd_str(cmd, dst, arg))
+        msg(1, 'exec:', cmd_str(cmd, dst, arg))
         func = CMDS.get(cmd, None)
         if func:
             df = func(df, dst, *arg)
@@ -372,7 +511,6 @@ def main():
             msg(0, '-'*65, 'bye :(')
             sys.exit(1)
 
-    print('-'*80)
     write_csv(df)
 
     return 0
